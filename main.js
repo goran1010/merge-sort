f59b5dcc64dbfc6bd886c908ddc06cf226c9ac70
Goran Jovic <goran1010jovic@gmail.com>
2023-11-04 17:24:18 +0100

    Finished

---
+++ main.js
@@ -0,0 +1,33 @@
+const array = [545, 17, 2, 8, 224, 35, 1000];
+
+function merge(arrayOne, arrayTwo) {
+  let i = 0;
+  let j = 0;
+  const sortedArray = [];
+  while (i < arrayOne.length && j < arrayTwo.length) {
+    if (arrayOne[i] < arrayTwo[j]) {
+      sortedArray.push(arrayOne[i]);
+      i++;
+    } else {
+      sortedArray.push(arrayTwo[j]);
+      j++;
+    }
+  }
+  for (; i < arrayOne.length; i++) {
+    sortedArray.push(arrayOne[i]);
+  }
+  for (; j < arrayTwo.length; j++) {
+    sortedArray.push(arrayTwo[j]);
+  }
+  return sortedArray;
+}
+
+function mergeSort(array, first = 0, last = array.length - 1) {
+  if (first < last) {
+    let mid = Math.floor((first + last) / 2);
+    return merge(mergeSort(array, first, mid), mergeSort(array, mid + 1, last));
+  }
+  return [array[first]];
+}
+
+console.log(mergeSort(array));
